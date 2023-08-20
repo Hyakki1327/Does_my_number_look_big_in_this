20a61bf0022ebb6a200d489534d5b054bd562ee4
hyakki1327 <hyakki1327@gmail.com>
2023-08-20 11:03:04 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿var number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Narcissistic(number));
+
+static bool Narcissistic(int value)
+{
+    double result = 0;
+    var tempValue = value;
+
+    for (int i = 0; i < value.ToString().Length; i++)
+    {
+        result += Math.Pow(tempValue % 10, value.ToString().Length);
+        tempValue /= 10;
+    }
+
+    if (value == result)
+        return true;
+    else
+        return false;
+}
